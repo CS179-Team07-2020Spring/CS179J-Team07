9ba990faaa53d260bc279a7ae9ad3bee27786981
eunc <eunc1996@gmail.com>
2020-04-29 20:38:54 -0700

    added action space/observation space and a more detailed description of the env

--- code/gym-env/gym_env/envs/robot_env.py
+++ code/gym-env/gym_env/envs/robot_env.py
@@ -4,18 +4,44 @@ from gym import utils
 from gym.utils import seeding
 
 class RobotEnv(gym.Env):
+    """
+    Observation:
+        Type: Box(3)
+        Num     Observation     Min     Max
+        0       Red             0       255
+        1       Green           0       255
+        2       Blue            0       255
+
+    Actions:
+        Type: Discrete(3)
+        Num     Action
+        0       Go straight
+        1       Turn left
+        2       Turn right
+
+    Reward:
+        1 for every step taken without slowing down
+
+    Episode Termination:
+        Robot comes to a stop
+        Speed slows down
+
+    """
     metadata = {'render.modes': ['human']}
 
     @TODO
     def __init__(self):
         super(RobotEnv, self).__init__()
+        n_actions = 3
+        self.action_space = spaces.Discrete(n_actions)
+        self.observation_space = spaces.Box(0, 255, [height, width, 3])
 
     def step(self, action):
         """
         execute one time step within the environment
 
         :action: TODO
-        :returns: observation, reward, done, info
+        :returns: next observation, the immediate reward, if episode is done, additional info
         """
         return observation, reward, done, info
 
