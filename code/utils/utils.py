744b2042d863e7b54dbe3cbc35a7af34bc5359b9
zhouxuan009 <zhouxuan009@gmail.com>
2020-05-09 13:22:03 -0700

    data2

--- code/utils/utils.py
+++ code/utils/utils.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import torch
 import torchvision
+import torch.nn.functional as F
+import time
 
 # input: camera.value; output: nparray [H,W,C]
 def cameraToNparray(camera_value):
@@ -16,3 +18,27 @@ def nparrayToTorch(image_array):
 	x = torch.from_numpy(image_array).float()
 	return x
 
+
+
+# can be put into a block
+def update(change):
+    global blocked_slider, robot
+    x = change['new'] 
+    x = preprocess(x)
+    y = model(x)
+    
+    # we apply the `softmax` function to normalize the output vector so it sums to 1 (which makes it a probability distribution)
+    y = F.softmax(y, dim=1)
+    
+    prob_blocked = float(y.flatten()[0])
+    
+    blocked_slider.value = prob_blocked
+    
+    if prob_blocked < 0.5:
+        robot.forward(0.4)
+    else:
+        robot.left(0.4)
+    
+    time.sleep(0.001)
+        
+update({'new': camera.value})  # we call the function once to intialize
---
+++ data/readme.md
@@ -0,0 +1,5 @@
+
+
+image2.png is the image of the ground
+
+imageNParray2.npy is its corresponding numpy array file. The dimension of the array is [224, 224, 3]
