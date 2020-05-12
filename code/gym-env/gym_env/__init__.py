9a9a3861d60ff8da96591196b7e1200999deb52a
eunc <eunc1996@gmail.com>
2020-05-12 02:48:36 -0700

    import -> from

--- code/gym-env/gym_env/__init__.py
+++ code/gym-env/gym_env/__init__.py
@@ -1,9 +1,8 @@
-import logging
-import gym.envs.registration import register
-
-logger = logging.getLogger(__name__)
+from gym.envs.registration import register
 
 register(
         id='Robot-v0',
         entry_point='gym_env.envs:RobotEnv'
+        max_episode_steps=200
+        reward_threshold=195.0
 )
