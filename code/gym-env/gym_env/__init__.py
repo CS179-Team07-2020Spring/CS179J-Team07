0161510795edd0e6a3f29adda331e4fdfe936dec
eunc <eunc1996@gmail.com>
2020-05-25 17:36:32 -0700

    updating hierarchy

--- code/gym-env/gym_env/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-import gym.envs.registration import register
-
-logger = logging.getLogger(__name__)
-
-register(
-        id='Robot-v0',
-        entry_point='gym_env.envs:RobotEnv'
-)
--- code/gym-env/gym_env/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_env.envs.robot_env import RobotEnv
